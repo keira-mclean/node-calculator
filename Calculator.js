75cf66574c06cc223110a43ab5bfa5c95ae89230
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-06-29 13:48:49 -0400

    finished calculator app

--- Calculator.js
+++ Calculator.js
@@ -1,37 +1,28 @@
-let readline = require('readline-sync');
+class Calculator {
 
-let Calculation = function(num1, num2, op) {
-  this.x = parseInt(num1);
-  this.y = parseInt(num2);
-  this.op = op;
+    add(a, b) {
+        return a + b;
+    };
 
-  this.result = function() {
-    let x = this.x;
-    let y = this.y;
-    let op = this.op;
+    subtract(a, b) {
+        return a - b;
+    };
 
-    if (op === "+") {
-      result = x + y;
-    } else if (op === "-") {
-      result = x - y;
-    } else if (op === "/") {
-      result = x / y;
-    } else if (op === "*") {
-      result = x * y;
-    } else if (op === "%") {
-      result = x % y;
-    } else {
-      result = "Error!"
+    multiply(a, b) {
+        return a * b;
+    };
+
+    divide(a, b) {
+        return a / b;
+    };
+
+    findAreaOfCircle(radius, pi) {
+        return pi * radius * radius;
+    };
+
+    findAreaOfRectangle(side1, side2) {
+        return side1 * side2;
     }
-
-    console.log("Result: " + result);
-  };
 }
 
-let num1 = readline.question("Please enter a number: ");
-let num2 = readline.question("Please enter another number: ");
-let op = readline.question("Please enter an operator: ");
-
-let calc = new Calculation(num1, num2, op);
-
-calc.result();
+module.exports = Calculator;
--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-import Calculator from './Calculator';
+let Calculator = require('./Calculator.js')
 
+let calculate = new Calculator();
+
+let a = 15;
+let b = 4;
+let radius = 3.75;
+let pi = Math.PI;
+let side1 = 6;
+let side2 = 23;
+
+
+let add = calculate.add(a, b);
+console.log("Add "+a+" + "+b+" = " +add);
+
+let subtract = calculate.subtract(a, b);
+console.log("Subtract "+a+" - "+b+" = " +subtract);
+
+let multiply = calculate.multiply(a, b);
+console.log("Multiply "+a+" * "+b+" = " +multiply);
+
+let divide = calculate.divide(a, b);
+console.log("Divide "+a+" / "+b+" = " +divide);
+
+let findAreaOfCircle = calculate.findAreaOfCircle(radius, pi);
+console.log("The area of the circle with radius of "+radius+ " is " + findAreaOfCircle);
+
+let findAreaOfRectangle = calculate.findAreaOfRectangle(side1, side2);
+console.log("The area of a Rectangle with a width of "+side1+" and length of "+side2+" is "+findAreaOfRectangle);
